450b44152c6b818ea9b40759f5e0f4c4295a41bd
YL <simon-ling@hotmail.com>
2021-05-26 10:12:13 -0400

    Done w/ t15

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,20 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use Pizza model
+use App\Pizza;
 
 class PizzaController extends Controller
 {
     public function index(){
-        $pizzas=[
-            ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-            ['type' => 'volcano', 'base' => 'garlic crust'],
-            ['type' => 'veg supreme', 'base' => 'thin & crispy']       
-        ];
-        // query parameters
-        return view('pizzas',['pizzas'=>$pizzas,'name'=>request('name'),'age'=>request('age')]);
-        // return view('pizzas',['type'=>'hawaiin','base'=>'cheesy crust']);
-        // return 'pizzas';
-        // return ["name"=>"veg pizzas",'base'=>'classic'];
+        // $pizzas=Pizza::all();
+        // $pizzas=Pizza::orderBy('name','desc')->get();
+        // $pizzas=Pizza::where('type','hawaiian')->get();
+        $pizzas=Pizza::latest()->get(); // list the latest created_at
+
+        return view('pizzas',['pizzas'=>$pizzas]);        
     }
     public function show($id){
         return view('details',['id'=>$id]);
--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -6,22 +6,10 @@
         <div class="title m-b-md">
             Pizzas List
         </div>
-        <p>{{ $name }} - {{ $age }}</p>
-        {{-- @for ($i=0;$i<5;$i++)
-            <p>$i= {{ $i }}</p>
-        @endfor --}}
-        {{-- @for ($i=0;$i<count($pizzas);$i++)
-            <p>{{ $pizzas[$i]['type'] }}</p>
-        @endfor --}}
+        
         @foreach ($pizzas as $pizza)
             <div>
-                {{ $loop->index }}: {{ $pizza['type'] }} - {{ $pizza['base'] }}
-                @if ($loop->first)
-                    <span>- first in the loop</span>
-                @endif
-                @if ($loop->last)
-                    <span>- last in the loop</span>
-                @endif
+                {{ $pizza->name }} - {{ $pizza->type }} - {{ $pizza->base }} 
             </div>
         @endforeach
     </div>
