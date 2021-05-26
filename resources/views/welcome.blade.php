f920cc58506ba2f815ba6bf7c477ddfcd87c2171
YL <simon-ling@hotmail.com>
2021-05-25 21:04:11 -0400

    Done w/ t8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,21 +16,10 @@
     @endif
 
     <div class="content">
-        <div class="title m-b-md">
-            Pizza House<br />
+        <img src="/img/pizza-house.png" alt="pizza house logo">
+        <div class="title m-b-md">            
             The North's Best Pizzas
-        </div>
-
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
-        </div>
+        </div>        
     </div>
 </div>
 @endsection
